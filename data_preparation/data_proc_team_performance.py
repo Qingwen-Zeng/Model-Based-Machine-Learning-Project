3c2f33694bd1bc331e03ba7f0c5f3c3c942f556f
jofreb <s240661@dtu.dk>
2025-05-18 19:23:43 +0200

    fix yearly perf

--- data_preparation/data_proc_team_performance.py
+++ data_preparation/data_proc_team_performance.py
@@ -4,7 +4,7 @@ path_folder = './original_data'
 att_df = pd.read_csv(path_folder+'/attendance.csv')
 games_df = pd.read_csv(path_folder+'/games.csv')
 stands_df = pd.read_csv(path_folder+'/standings.csv')
-attendance_proc_df = pd.read_csv('./processed_data/attendance.csv')
+attendance_proc_df = pd.read_csv('./processed_data/attendance_capacity_rate.csv')
 
 games_df.loc[:, 'Winner Name'] = games_df['winner'].apply(lambda x: x.split(' ')[-1])
 
@@ -24,12 +24,12 @@ superbowl_winners = stands_df.loc[stands_df['sb_winner'] == 'Won Superbowl']
 playoffs_finishers = stands_df.loc[(stands_df['playoffs'] == 'Playoffs')]
 
 # True if they won the superbowl last year
-att_df.loc[:, 'Superbowl winners last year'] = att_df.apply(lambda x: superbowl_winners.loc[(superbowl_winners['year'] == x['year']-1) & (superbowl_winners['team_name'] == x['team_name'])].shape[0], axis=1)
+att_df.loc[:, 'Superbowl winners last year'] = att_df.apply(lambda x: superbowl_winners.loc[(superbowl_winners['year'] == x['year']-1) & (superbowl_winners['team_name'] == x['team_name'])].shape[0]==1, axis=1)
 # True if they won the superbowl 2 or 3 years before
 att_df.loc[:, 'Superbowl winners prev years'] = att_df.apply(lambda x: superbowl_winners.loc[((superbowl_winners['year'] == x['year']-2) | (superbowl_winners['year'] == x['year']-3)) & (superbowl_winners['team_name'] == x['team_name'])].shape[0]>0, axis=1)
 # True if they finished in playoffs during the last 2 years
 att_df.loc[:, 'Playoff finishers prev years'] = att_df.apply(lambda x: playoffs_finishers.loc[(playoffs_finishers['year'] < x['year']) & (playoffs_finishers['year'] >= x['year']-2) & (playoffs_finishers['team_name'] == x['team_name'])].shape[0]>0, axis=1)
 
 
-attendance_proc_df.loc[:, ['Sb_winners', 'Sb_winners_prev_years', 'Playoff_prev_years', 'Bad_streak', 'Medium_streak', 'Good_streak']] = att_df.loc[:, ['Superbowl winners last year', 'Superbowl winners prev years', 'Playoff finishers prev years', 'Bad_streak', 'Medium_streak', 'Good_streak']]
+attendance_proc_df.loc[:, ['Sb_winners', 'Sb_winners_prev_years', 'Playoff_prev_years', 'Bad_streak', 'Medium_streak', 'Good_streak']] = att_df.loc[:, ['Superbowl winners last year', 'Superbowl winners prev years', 'Playoff finishers prev years', 'Bad_streak', 'Medium_streak', 'Good_streak']].values
 attendance_proc_df.to_csv('./processed_data/attendance_w_team_perf.csv', index=False)
